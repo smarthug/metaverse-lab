839097b4fd66ef2bac228796d78d331c6ebbeb80
hosuk <kirklayer@gmail.com>
2021-08-24 16:54:51 +0900

    pubsub init

---
+++ src/Page/IpfsPubSubTest.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import {create} from 'ipfs-http-client'
+
+export default function Main(){
+
+
+    useEffect(() => {
+        // console.log(aaa)
+
+    },[])
+
+    return(
+        <div>
+            <h1>IPFS PubSub Test</h1>
+        </div>
+    )
+}
--- src/Page/index.js
+++ src/Page/index.js
@@ -2,3 +2,4 @@
 // export * as CashCannon from './cashCannon'
 export * as MusicExperience from './musicExperience'
 // export * as MusicTest from './musicTest'
+export * as IPFSPubSubTest from './IpfsPubSubTest'
