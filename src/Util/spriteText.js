103fabfbd4d0a51708e51c441c4fc3caeae0c511
dem <dem1455@naver.com>
2021-09-23 13:00:29 +0900

    sprite Name

---
+++ src/Util/spriteText.js
@@ -0,0 +1,17 @@
+import SpriteText from "three-spritetext";
+import * as THREE from 'three'
+
+//text = [ 물산업 혁신처, 이용우 부장, 수자원공사 ]
+export function name(text) {
+    let nameSet = new THREE.Group();
+
+    // let text;
+    let textHeight = 10
+    let textColor = "white"
+    for (let i in text) {
+        let name = new SpriteText(text[i], textHeight, textColor)
+        name.backgroundColor = "black"
+        nameSet.add(name);
+    }
+    return nameSet;
+}
